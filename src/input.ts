66fbfc7c213bfc59e1d8a1c616ffa476420a9f7f
Michael Engel <engel.michael@hotmail.de>
2022-11-23 12:17:22 +0100

    updated dist

--- src/input.ts
+++ src/input.ts
@@ -3,25 +3,25 @@ import * as core from "@actions/core";
 export interface Inputs {
   limgoFile: string;
   limgoFileFormat: string;
-  colorSuccess: string;
-  colorFailure: string;
   badgeName: string;
   repo: string;
   branch: string;
   githubToken: string;
   githubTokenOwner: string;
+  colorSuccess: string;
+  colorFailure: string;
 }
 
 export async function getInputs(): Promise<Inputs> {
   return {
     limgoFile: core.getInput("limgo_file"),
     limgoFileFormat: core.getInput("limgo_file_format"),
-    colorSuccess: core.getInput("color_success"),
-    colorFailure: core.getInput("color_failure"),
     badgeName: core.getInput("badge_name"),
     repo: core.getInput("repo"),
     branch: core.getInput("branch"),
     githubToken: core.getInput("github_token"),
     githubTokenOwner: core.getInput("github_token_owner"),
+    colorSuccess: core.getInput("color_success"),
+    colorFailure: core.getInput("color_failure"),
   };
 }
