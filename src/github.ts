0f294d18cb6a6c68429c0b72da6f9d7014e29865
Michael Engel <engel.michael@hotmail.de>
2022-11-23 14:17:39 +0100

    added diff check

--- src/github.ts
+++ src/github.ts
@@ -30,6 +30,10 @@ export async function updateBadgeInRepo(
   await ghexec.exec(
     `cp -f ${badgePath} ./${tmpRepoDir}/${badgeFileTargetName}`
   );
+  const diff = await ghexec.getExecOutput(assembleGitCmd(tmpRepoDir, `diff`));
+  if (diff.stdout == "") {
+    console.log("No changes detected, skipping...")
+  }
   await ghexec.exec(assembleGitCmd(tmpRepoDir, `add ${badgeFileTargetName}`));
   await ghexec.exec(
     assembleGitCmd(tmpRepoDir, `commit -m "updated limgo badge"`)
